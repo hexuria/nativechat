0599fd62d96695773b284b013ed7f13fd14596e9
codeitlikemiley <codeitlikemiley@gmail.com>
2025-11-22 06:53:42 +0800

    fix tooltip

--- src/components/message_actions.rs
+++ src/components/message_actions.rs
@@ -1,72 +1,59 @@
 use gpui::*;
-use gpui_component::{
-    IconName, Sizable,
-    button::{Button, ButtonVariants},
-    h_flex,
-};
+use gpui_component::{ActiveTheme, Icon, IconName, h_flex, tooltip::Tooltip};
 
 #[derive(IntoElement)]
 pub struct MessageActions {
-    message_id: String,
+    _message_id: String,
 }
 
 impl MessageActions {
     pub fn new(message_id: impl Into<String>) -> Self {
         Self {
-            message_id: message_id.into(),
+            _message_id: message_id.into(),
         }
+    }
+
+    fn action_button(
+        &self,
+        id: impl Into<ElementId>,
+        icon: IconName,
+        tooltip_text: &'static str,
+        cx: &mut App,
+    ) -> impl IntoElement {
+        let theme = cx.theme();
+        let secondary = theme.secondary;
+        let secondary_foreground = theme.secondary_foreground;
+
+        div()
+            .id(id)
+            .w(px(32.0))
+            .h(px(32.0))
+            .flex()
+            .items_center()
+            .justify_center()
+            .rounded(px(6.0))
+            .text_color(secondary_foreground)
+            .hover(move |style| style.bg(secondary))
+            .cursor_pointer()
+            .tooltip(move |w, cx| Tooltip::new(tooltip_text).build(w, cx))
+            .child(
+                Icon::new(icon)
+                    .size(px(16.0))
+                    .text_color(secondary_foreground),
+            )
     }
 }
 
 impl RenderOnce for MessageActions {
-    fn render(self, _window: &mut Window, _cx: &mut App) -> impl IntoElement {
-        // Use hash of message_id as unique number for ElementId
-        let id_hash = self.message_id.len(); // Simple hash for now
-
+    fn render(self, _window: &mut Window, cx: &mut App) -> impl IntoElement {
         h_flex()
             .gap_1()
             .items_center()
-            .child(
-                Button::new(("copy", id_hash))
-                    .icon(IconName::Copy)
-                    .ghost()
-                    .xsmall()
-                    .tooltip("Copy"),
-            )
-            .child(
-                Button::new(("like", id_hash))
-                    .icon(IconName::ThumbsUp)
-                    .ghost()
-                    .xsmall()
-                    .tooltip("Good response"),
-            )
-            .child(
-                Button::new(("dislike", id_hash))
-                    .icon(IconName::ThumbsDown)
-                    .ghost()
-                    .xsmall()
-                    .tooltip("Bad response"),
-            )
-            .child(
-                Button::new(("share", id_hash))
-                    .icon(IconName::ArrowUp)
-                    .ghost()
-                    .xsmall()
-                    .tooltip("Share"),
-            )
-            .child(
-                Button::new(("regenerate", id_hash))
-                    .icon(IconName::Settings)
-                    .ghost()
-                    .xsmall()
-                    .tooltip("Try again"),
-            )
-            .child(
-                Button::new(("more", id_hash))
-                    .icon(IconName::Menu)
-                    .ghost()
-                    .xsmall()
-                    .tooltip("More actions"),
-            )
+            .child(self.action_button("copy", IconName::Copy, "Copy", cx))
+            .child(self.action_button("like", IconName::ThumbsUp, "Good response", cx))
+            .child(self.action_button("dislike", IconName::ThumbsDown, "Bad response", cx))
+            .child(self.action_button("share", IconName::ArrowUp, "Share", cx))
+            .child(self.action_button("regenerate", IconName::Settings, "Try again", cx))
+            .child(self.action_button("more", IconName::Menu, "More actions", cx))
     }
 }
